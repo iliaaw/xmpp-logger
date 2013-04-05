7f38e0e2b5c82eb19ad690336c1f6d49948687e4
Ilya <ilyahohryakov@gmail.com>
2013-04-05 15:00:40 +0400

    Added a helper to add leazing zero

--- helpers/html.rb
+++ helpers/html.rb
@@ -7,5 +7,10 @@ class App < Sinatra::Base
     def last_day_href
       Message.last.created_at.strftime("/%Y/%m/%d")
     end
+
+    def lz(number)
+      # lz means 'leading zero'
+      "%02d" % number.to_i
+    end
   end
 end
