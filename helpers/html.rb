63e5444598cab2e86069952535962f02581a798b
Ilya <ilyahohryakov@gmail.com>
2013-04-05 14:43:46 +0400

    Implemented a link to last day

--- helpers/html.rb
+++ helpers/html.rb
@@ -3,5 +3,9 @@ class App < Sinatra::Base
     def html_escape(text)
       Rack::Utils.escape_html(text)
     end
+
+    def last_day_href
+      Message.last.created_at.strftime("/%Y/%m/%d")
+    end
   end
 end
