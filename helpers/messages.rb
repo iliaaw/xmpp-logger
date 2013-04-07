58303386ffc02a2e8273293d58a9e3ab2ea36fb1
Ilya <ilyahohryakov@gmail.com>
2013-04-08 03:45:37 +0400

    Improved a helper for links processing

--- helpers/messages.rb
+++ helpers/messages.rb
@@ -1,9 +1,8 @@
 class App < Sinatra::Base
   helpers do
     def escape_text_message(message)
-      link_regex = /(http|https|ftp|ftps)\:&#x2F;&#x2F;[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(&#x2F;\S*)?/
       message = Rack::Utils.escape_html(message)
-      message.gsub!(/(http:&#x2F;&#x2F;\S+)/, '<a href="\1">\1</a>')
+      message.gsub!(/(https?:&#x2F;&#x2F;\S+)/, '<a href="\1">\1</a>')
       message.gsub!(/\n/, '<br />')
       message
     end
