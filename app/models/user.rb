62e85353b0775882e02ed6c624eb027d19aad499
Ilya <ilyahohryakov@gmail.com>
2013-08-20 19:39:49 +0600

    A bit of refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate(login, password)
     user = find_by_login(login)
-    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-      user
-    end
+    user if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
   end
 
   def encrypt_password 
